6899dec988aa88423375fc897bc7a6963c28300d
HoangTrong <hoangcongtrong054@gmail.com>
2023-02-08 16:43:26 +0700

    first commit

---
+++ CSharpFundamental/DirectoryAndDirectoryInfo/DirectoryAndDirectoryInfo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CSharpFundamental
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Directory.CreateDirectory(@"c:\temp\folder");
+            var files = Directory.GetFiles(@"c:\project\CSharpFundamental", "*.*", SearchOption.AllDirectories);
+            foreach (var file in files)
+            {
+                Console.WriteLine(file);
+            }
+
+            var directories = Directory.GetDirectories(@"c:\project\CSharpFundamental", "*.*", SearchOption.AllDirectories);
+            foreach (var directory in directories)
+            {
+                Console.WriteLine(directory);
+            }
+
+            Directory.Exists("...");
+
+            var directoryInfo = new DirectoryInfo("...");
+            directoryInfo.GetFiles();
+            directoryInfo.GetDirectories();
+        }
+    }
+}
---
+++ CSharpFundamental/FileAndFileIO/FileAndFileIO/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace CSharpFundamental
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"c:\somefile.jpg";
+            File.Copy(@"c:\Temp\myfile.jpg", @"d:\temp\myfile.jpg", true);
+            File.Delete(path);
+            if(File.Exists(path) )
+            {
+                //
+            }
+
+            var content = File.ReadAllText(path);
+
+            var fileInfo = new FileInfo(path);
+            fileInfo.CopyTo("...");
+            fileInfo.Delete();
+            if(fileInfo.Exists )
+            {
+                //
+            }
+        }
+    }
+}
